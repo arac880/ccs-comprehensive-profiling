c9ab8370994810e92f2607ec25e0305738bbaa06
esang222 <carinagajessa@gmail.com>
2026-03-19 23:23:27 +0800

    Student Schedule

--- ccs-cps-frontend/src/App.jsx
+++ ccs-cps-frontend/src/App.jsx
@@ -7,6 +7,7 @@ import {
 import Login from "./pages/Login";
 import FacultyDashboard from "./pages/facultyPages/facultyDashboard";
 import StudentDashboard from "./pages/studentPages/studentDashboard";
+import StudentSchedule from "./pages/studentPages/studentSchedule";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/faculty/dashboard" element={<FacultyDashboard />} />
         <Route path="/student/dashboard" element={<StudentDashboard />} />
+        <Route path="/student/schedule" element={<StudentSchedule />} />
         <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
     </Router>
