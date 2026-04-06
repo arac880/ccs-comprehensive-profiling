d580451190ebc9253f78794ce45b7908de00f1dc
arac880 <ceresarachristina74@gmail.com>
2026-04-06 11:58:12 +0800

    fix server config for render

--- ccs-cps-backend/server.js
+++ ccs-cps-backend/server.js
@@ -1,5 +1,5 @@
-const dns = require("dns");
-dns.setServers(["8.8.8.8", "8.8.4.4"]);
+// const dns = require("dns");
+// dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 const express = require("express");
 const cors = require("cors");
@@ -12,7 +12,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors()); // Allows frontend to make requests to this backend
+app.use(
+  cors({
+    origin:
+      process.env.FRONTEND_URL ||
+      "https://ccs-comprehensive-profiling-lkdtdtsn7.vercel.app",
+  }),
+); // Allows frontend to make requests to this backend
 app.use(express.json()); // Allows the server to accept JSON data in the req.body
 
 // Connect to Database
@@ -29,5 +35,5 @@ app.get("/", (req, res) => {
 
 // Start Server
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on PORT ${PORT}`);
 });
