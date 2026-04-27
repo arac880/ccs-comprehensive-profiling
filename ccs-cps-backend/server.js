0fe22512037a5fd3b669ef967c5eccf59f24749d
arac880 <ceresarachristina74@gmail.com>
2026-04-27 14:56:34 +0800

    changes on server. js

--- ccs-cps-backend/server.js
+++ ccs-cps-backend/server.js
@@ -20,9 +20,9 @@ const io = new Server(server, {
   cors: {
     origin: process.env.CLIENT_URL || "http://localhost:5173",
     methods: ["GET", "POST"],
+    credentials: true,
   },
 });
-
 // Make socket available globally
 global.io = io;
 
@@ -44,8 +44,12 @@ io.on("connection", (socket) => {
 app.use(
   cors({
     origin: process.env.CLIENT_URL || "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
