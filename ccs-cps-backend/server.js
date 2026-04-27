9b3a5645e4c4a918164d99850675313c272c43e4
arac880 <ceresarachristina74@gmail.com>
2026-04-27 10:53:38 +0800

    may inedit kay server

--- ccs-cps-backend/server.js
+++ ccs-cps-backend/server.js
@@ -77,9 +77,9 @@ const startServer = async () => {
 };
 
 // ─── Serve React Frontend (IMPORTANT FOR REFRESH FIX) ───
-app.use(express.static(path.join(__dirname, "build")));
+// app.use(express.static(path.join(__dirname, "build")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "build", "index.html"));
+// });
 startServer();
